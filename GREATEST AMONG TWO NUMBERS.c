e02ca751e3a215324bd148601d6d3f90f1d10d23
Saswata Sundar Naskar <144191079+syntosin@users.noreply.github.com>
2023-10-08 19:55:03 +0530

    Add files via upload

---
+++ GREATEST AMONG TWO NUMBERS.c
@@ -0,0 +1,18 @@
+// WAP IN C TO FIND THE GREATEST AMONG TWO NUMBERS
+
+#include<stdio.h>
+int main(){
+	int num1,num2;
+	printf("ENTER NUMBER ONE :\n");
+	scanf("%d", &num1);
+	printf("ENTER NUMBER TWO :\n");
+	scanf("%d", &num2);
+	if(num1>num2){
+		printf("NUMBER ONE IS GREATEST");
+		
+	}
+	else{
+		printf("SECOND NUMBER IS GREATEST");
+	}
+	
+}
