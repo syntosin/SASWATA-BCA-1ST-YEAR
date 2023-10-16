dbabf3063423f9d4fb5d184f441794d51e5c7a51
Saswata Sundar Naskar <144191079+syntosin@users.noreply.github.com>
2023-10-16 21:21:46 +0530

    Add files via upload

---
+++ INTRODUCTION TO C PROGRAMMING/FARENHEIGHT TO CELSIUS CONVERSION.c
@@ -0,0 +1,21 @@
+//WAP IN C TO DEMONSTRATE THE FARENHEIGHT TO CELSIUS CONVERSION
+
+#include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius;
+
+    
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    
+    printf("Temperature in Celsius: %.2f\n", celsius);
+
+    return 0;
+}
+
+
