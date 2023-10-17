4e407434bdddf3c7c35e838590be59d2d99c658a
Saswata Sundar Naskar <144191079+syntosin@users.noreply.github.com>
2023-10-17 23:44:04 +0530

    Add files via upload

---
+++ INTRODUCTION TO C PROGRAMMING/THE NUMBER IS ODD OR EVEN.c
@@ -0,0 +1,16 @@
+// WAP IN C TO CHECK THE NUMBER IS ODD OR EVEN
+
+#include<stdio.h>
+int main(){
+	int num;
+	printf("ENTER A NUMBER ");
+	scanf("%d", &num);
+	if(num%2==0){
+		printf("THE NUMBER IS EVEN");
+		
+	}
+	else{
+		printf("THE NUMBER IS ODD ");
+	}
+}
+
