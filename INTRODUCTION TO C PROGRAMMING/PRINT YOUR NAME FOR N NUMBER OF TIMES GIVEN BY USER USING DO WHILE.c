20798e584cac2ede429ed68cd099dc5981cb72db
Saswata Sundar Naskar <144191079+syntosin@users.noreply.github.com>
2023-10-18 23:35:55 +0530

    Add files via upload

---
+++ INTRODUCTION TO C PROGRAMMING/PRINT YOUR NAME FOR N NUMBER OF TIMES GIVEN BY USER USING DO WHILE.c
@@ -0,0 +1,15 @@
+// WAP IN C TO PRINT YOUR NAME FOR N NUMBER OF TIMES GIVEN BY USER USING DO WHILE
+
+#include<stdio.h>
+int main(){
+	int start,n;
+	printf("ENTER THE VALUE OF N :");
+	scanf("%d", &n);
+	do{
+		printf("MY NAME IS SASWATA \n");
+	start++;
+	}
+	while(start<=n);
+
+}
+
